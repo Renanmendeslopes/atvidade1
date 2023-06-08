0a60e714f3cc7051a1175d9c737197e8df8953c6
Renanmendeslopes <134557324+Renanmendeslopes@users.noreply.github.com>
2023-06-08 14:25:01 -0300

    Add files via upload

---
+++ programas php/atvd1.php
@@ -0,0 +1,24 @@
+<?php
+$valor1 = 10;
+$valor2 = 3;
+
+
+$resultado = $valor1 + $valor2;
+echo "Adição: " . $resultado . "<br>";
+
+
+$resultado = $valor1 - $valor2;
+echo "Subtração: " . $resultado . "<br>";
+
+
+$resultado = $valor1 / $valor2;
+echo "Divisão: " . $resultado . "<br>";
+
+
+$resultado = $valor1 * $valor2;
+echo "Multiplicação: " . $resultado . "<br>";
+
+
+$resultado = $valor1 % $valor2;
+echo "Resto da divisão: " . $resultado . "<br>";
+?>
